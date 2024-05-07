b76e252a860cf136f95c7f9c8d71b8533d1119f4
leejinwoo <leejinwoo1126@gmail.com>
2024-05-07 21:32:08 +0900

    feat: 헤더 스타일 적용

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React, {useState} from "react";
+import styles from './Header.module.css';
 
 export default function Header({filters, filter, onFilterChange}) {
     return (
-        <header>
-            <ul>
+        <header className={styles.header}>
+            <ul className={styles.filters}>
             {
                 filters.map((value, index) => (
                     <li key={index}>
-                        <button onClick={() => onFilterChange(value)}>{value}</button>
+                        <button className={`${styles.filter} ${filter === value && styles.selected}`}
+                                onClick={() => onFilterChange(value)}>
+                            {value}</button>
                     </li>
                 ))
             }
